3fbd3c356dd920b6a6c31189bb1b1bbf127ce7bf
Marlon Strong <tsenane14@gmail.com>
2021-06-10 22:40:20 +0200

    adding folder styles

--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native'
 
 export default function Welcome() {
     return (
-        <View>
-            <Text>Manage your plants easily</Text>
-        </View>
+        <SafeAreaView style={styles.container} >
+            <Text style={styles.title} >Manage your plants easily</Text>
+            <Image source={require('../assets/watering.png')}/>
+            <Text style={styles.subtitle} >Don't forget to water your plants. We will remember you whener you need it.</Text>
+            <TouchableOpacity style={styles.button} >
+                <Text>GO</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'space-between',
+    },
+    title:{
+        fontSize:32,
+        fontWeight:'bold',
+        textAlign:'center',
+        
+    },
+    subtitle:{
+
+    },
+    button:{
+
+    },
+})
