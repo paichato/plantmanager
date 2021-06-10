da819177a73c2962ddff593d03241130f4a091c4
Marlon Strong <tsenane14@gmail.com>
2021-06-10 23:53:45 +0200

    added font loader

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './src/screens/welcome';
+import {useFonts, Jost_400Regular,Jost_600SemiBold} from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded]=useFonts({
+    Jost_400Regular,Jost_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
