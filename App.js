27c78fba533dedebe078b8da85f41bdfb7b24882
Marlon Strong <tsenane14@gmail.com>
2021-06-12 13:11:18 +0200

    Added styles to  userIdentification.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './src/screens/welcome';
 import {useFonts, Jost_400Regular,Jost_600SemiBold,Jost_700Bold} from '@expo-google-fonts/jost'
 import AppLoading from 'expo-app-loading';
+import UserIdentification from './src/screens/userIdentification';
 
 export default function App() {
   const [fontsLoaded]=useFonts({
@@ -17,7 +18,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Welcome/>
+      {/* <Welcome/> */}
+      <UserIdentification/>
       
     </View>
   );
