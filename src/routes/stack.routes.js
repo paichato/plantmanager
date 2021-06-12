200c4d7e4b79b89e2bfdc51393c7cfae0072b399
Marlon Strong <tsenane14@gmail.com>
2021-06-12 17:04:21 +0200

    added navigation screens

---
+++ src/routes/stack.routes.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import {createStackNavigator} from '@react-navigation/stack'
+import colors from '../styles/colors';
+import Welcome from '../screens/welcome';
+import UserIdentification from '../screens/userIdentification';
+import Confirmation from '../screens/Confirmation';
+
+const StackRoutes=createStackNavigator();
+
+export default function AppRoutes() {
+    return (
+        <StackRoutes.Navigator headerMode="none"
+        screenOptions={{
+            cardStyle:{
+                backgroundColor:colors.white,
+            }
+        }}
+        >
+            <StackRoutes.Screen 
+            name="Welcome" component={Welcome}/>
+            <StackRoutes.Screen 
+            name="UserId" component={UserIdentification}/>
+            <StackRoutes.Screen 
+            name="Confirmation" component={Confirmation}/>
+
+            
+
+        </StackRoutes.Navigator>
+    )
+}
