95fc977ea0dfe4aaa3a93a9bf14bde66b94ebfa6
Marlon Strong <tsenane14@gmail.com>
2021-06-20 04:11:02 +0200

    Added envButton.js

---
+++ src/components/EnvButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+
+export default function EnvButton() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
