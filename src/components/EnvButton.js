a41ece45600d81612336d80ef2c39cbfa5934f63
Marlon Strong <tsenane14@gmail.com>
2021-06-20 04:58:38 +0200

    flatLisst done

--- src/components/EnvButton.js
+++ src/components/EnvButton.js
@@ -1,38 +1,42 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+// import { RectButton } from "react-native-gesture-handler";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
-import colors from '../styles/colors'
-import fonts from '../styles/fonts'
-
-export default function EnvButton({title, active=false,...rest}) {
-    return (
-        <RectButton style={[styles.container,active && styles.containerActive]} {...rest}>
-            <Text style={[styles.text, active && styles.textActive]} >{title}</Text>
-        </RectButton>
-    )
+export default function EnvButton({ title, active = false, ...rest }) {
+  return (
+    <RectButton
+      style={[styles.container, active && styles.containerActive]}
+      {...rest}
+    >
+      <Text style={[styles.text, active && styles.textActive]}>{title}</Text>
+    </RectButton>
+  );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        backgroundColor:colors.shape,
-        width: 76,
-        height: 40,
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:12,
-    },
-    text:{
-        color: colors.heading,
-        fontFamily:fonts.text,
-    },
-    containerActive:{
-        // fontFamily:fonts.heading,
-        backgroundColor: colors.green_light,
-        // color: colors.green,
-    },
-    textActive:{
-        fontFamily:fonts.heading,
-        color: colors.green_dark,
-    },
-})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.shape,
+    width: 76,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    marginRight: 5,
+  },
+  text: {
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  containerActive: {
+    // fontFamily:fonts.heading,
+    backgroundColor: colors.green_light,
+    // color: colors.green,
+  },
+  textActive: {
+    fontFamily: fonts.heading,
+    color: colors.green_dark,
+  },
+});
--- src/screens/PlantSelect.js
+++ src/screens/PlantSelect.js
@@ -1,48 +1,61 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import EnvButton from '../components/EnvButton'
-import { Header } from '../components/Header'
-import colors from '../styles/colors'
-import fonts from '../styles/fonts'
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import EnvButton from "../components/EnvButton";
+import { Header } from "../components/Header";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export default function PlantSelect() {
-    return (
-        <View style={styles.container} >
-           
-                <Header/>
-                <View style={styles.header} >
-                <Text style={styles.title} >On wich environment</Text>
-                <Text style={styles.subtitle} >you will place your plant? </Text>
-                </View>
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.header}>
+        <Text style={styles.title}>On wich environment</Text>
+        <Text style={styles.subtitle}>you will place your plant? </Text>
+      </View>
 
-                <EnvButton title="Cozinha" active />
-          
-        </View>
-    )
+      <View>
+        <FlatList
+          data={[1, 2, 3, 4, 5, 6, 7, 8]}
+          renderItem={({ item }) => <EnvButton title="Cozinha" active />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.envList}
+        />
+      </View>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor:colors.background,
-        marginTop:20,
-        // alignItems:'center',
-        // justifyContent:'center',
-    },
-    header:{
-        paddingHorizontal:20,
-    },
-    title:{
-        fontSize:17,
-        color:colors.heading,
-        fontFamily:fonts.heading,
-        lineHeight:20,
-        marginTop:15,
-    },
-    subtitle:{
-        fontFamily:fonts.text,
-        fontSize:17,
-        lineHeight:20,
-        color: colors.heading,
-    },
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    marginTop: 20,
+    // alignItems:'center',
+    // justifyContent:'center',
+  },
+  header: {
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  envList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
+});
