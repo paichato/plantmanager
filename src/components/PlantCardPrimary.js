5b6c3f16d8117bcea39ccf088186106c80933d60
Marlon Strong <tsenane14@gmail.com>
2021-06-20 07:06:27 +0200

    filter is working fine

---
+++ src/components/PlantCardPrimary.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+import { SvgFromUri } from "react-native-svg";
+
+export function PlantCardPrimary({ data, ...rest }) {
+  return (
+    <RectButton style={styles.container}>
+      <SvgFromUri width={70} height={70} uri={data.photo} />
+      <Text style={styles.text}>{data.name}</Text>
+    </RectButton>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    maxWidth: "45%",
+    backgroundColor: colors.shape,
+    borderRadius: 20,
+    paddingVertical: 10,
+    alignItems: "center",
+    margin: 10,
+  },
+  text: {
+    color: colors.green_dark,
+    fontFamily: fonts.heading,
+    marginVertical: 16,
+  },
+});
