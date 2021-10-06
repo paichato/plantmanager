a4885cb6718b6ff8499dee2c17727ff23fea116d
paichato <tsenane14@gmail.com>
2021-10-06 21:35:45 +0200

    added structure for notifications

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,8 +1,40 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { format } from "date-fns";
+import * as Notifications from "expo-notifications"
 
 export async function savePlant(plant) {
   try {
+
+
+    const nextTime = new Date(plant.dateTimeNotification);
+    const now=new Date();
+    const {times,repeat_every}=plant.frequency;
+    if(repeat_every ==='week'){
+      const interval=Math.trunc(7/times,1);
+      nextTime.setDate(now.getDate()+interval);
+    }else{
+      nextTime.setDate(nextTime.getDate()+1);
+    }
+
+    const seconds=Math.abs(Math.ceil(now.getTime()-nextTime.getTime()/1000));
+
+    //notifications start here
+    const notificationId=await Notifications.scheduleNotificationAsync({
+      content:{
+        title:'heeey 🪴',
+        body:`It's take to take care of your plant ${plant.name}`,
+        sound,
+        priority:Notifications.AndroidNotificationPriority.HIGH,
+        data:{
+          plant
+        },
+      },
+      trigger:{
+        seconds:seconds <60 ?60 :seconds,
+        repeats:true
+      }
+    })
+
     const data = await AsyncStorage.getItem("@plantmanager:plants");
     const oldPlants = data ? JSON.parse(data) : {};
 
